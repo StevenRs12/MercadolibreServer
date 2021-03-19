922c95e10b06581e37a0d1bd12cdf7808f1c7dd1
Alvaro.Rojas <Alvaro.Rojasb@ilumno.com>
2021-03-19 11:35:09 -0500

    script add

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-  PORT: 3000,
+  PORT: process.env.PORT || 3000,
   BASE_URL: '/stevenrojas/meli/api',
   BASE_URL_MELI: 'https://api.mercadolibre.com/items/',
   BASE_URL_MELI_MLA: 'https://api.mercadolibre.com/sites/MLA/',
